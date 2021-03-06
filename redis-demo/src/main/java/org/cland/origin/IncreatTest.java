8c747ffa7495b4855e0ad25c5020303fe3b4b677
hack0303 <1097037585@qq.com>
2021-03-06 17:22:38 +0800

    1.incr decr

--- redis-demo/src/main/java/org/cland/origin/IncreatTest.java
+++ redis-demo/src/main/java/org/cland/origin/IncreatTest.java
@@ -1,9 +1,21 @@
 package org.cland.origin;
 
-public class IncreatTest extends BaseSupport {
+import lombok.extern.slf4j.Slf4j;
 
-	public static void main(String[] args) {
-     
-	}
+@Slf4j
+public class IncreatTest extends BaseSupport {
+    public static String keyA = "key-a";
+    public static String msgTemplate = "{} now is {}";
+
+    public static void main(String[] args) {
+        log.info(msgTemplate, keyA, jd.incr(keyA));
+        log.info(msgTemplate, keyA, jd.incrBy(keyA, 10));
+        log.info(msgTemplate, keyA, jd.decr(keyA));
+        log.info(msgTemplate, keyA, jd.decrBy(keyA, 10));
+        Long opResult=jd.del(keyA);
+        if (opResult.equals(1L)) {
+            log.info("{} clean suc", keyA);
+        }
+    }
 
 }
