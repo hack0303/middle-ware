c18011e7f54acbba32a418389cc6c6aa66fea217
hack0303 <1097037585@qq.com>
2021-03-06 16:40:04 +0800

    1.添加日志

--- redis-demo/src/main/java/org/cland/origin/StringTest.java
+++ redis-demo/src/main/java/org/cland/origin/StringTest.java
@@ -1,13 +1,19 @@
 package org.cland.origin;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class StringTest extends BaseSupport {
 
 	public static void main(String[] args) {
 		 /*�п�*/
-	     jd.select(5);
+	     jd.select(0);
 	     jd.append("stringKey", "part1");
 	     jd.append("stringKey", "part2");
 	     jd.append("stringKey", "part3");
+	     //jd.set("expire5s","");
+	     log.info("{}","hello");
+
 	}
 
 }
