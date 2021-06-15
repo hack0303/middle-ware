d22b5ad8f6ca1497cd836e18bc2baa55481af74f
hack0303 <1097037585@qq.com>
2021-06-15 21:08:39 +0800

    1.mybatis plus
    Signed-off-by: hack0303 <1097037585@qq.com>

---
+++ rdb-demos/mysql-demo/sql/ds1_t_order0.sql
@@ -0,0 +1,9 @@
+create table t_order0
+(
+    order_id varchar(35) not null comment '订单号',
+    user_id  varchar(32) not null comment '用户ID'
+        primary key,
+    constraint t_order_id_uindex
+        unique (order_id)
+);
+
