030061afde9381870d86ad824332996957d47152
hack0303 <1097037585@qq.com>
2021-10-10 10:02:53 +0800

    1.memcached
    Signed-off-by: hack0303 <1097037585@qq.com>

--- cache-demos/memcached-demo/src/main/java/org/cland/Main.java
+++ cache-demos/memcached-demo/src/main/java/org/cland/Main.java
@@ -1,8 +1,16 @@
 package org.cland;
 
+import net.spy.memcached.MemcachedClient;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
 public class Main {
 
-    public static void main(String[] args){
+    static final String key="hello";
 
+    public static void main(String[] args) throws IOException {
+        MemcachedClient client=new MemcachedClient(new InetSocketAddress("node01",11211));
+        System.out.println(client.add(key,10,"hello-v").getStatus().isSuccess());
     }
 }
